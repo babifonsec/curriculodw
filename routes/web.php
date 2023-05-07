848663ebbe5bfc6c55086d9137fdfe6c64a5b68c
barbara <barbarafonsec4@gmail.com>
2023-05-07 10:38:42 -0300

    comit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about.blade.php', function () {
+    return view('about');
+});
+
+Route::get('/portfolio.blade.php', function () {
+    return view('portfolio');
+});
+
+Route::get('/index.blade.php', function () {
+    return view('index');
 });
